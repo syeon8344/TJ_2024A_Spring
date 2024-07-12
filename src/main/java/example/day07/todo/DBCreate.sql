d62828c4afbad80246d217cd95f4690e8bc983d4
syeon8344 <yeon8344@gmail.com>
2024-07-12 16:59:37 +0900

    0712 3:54PM

--- src/main/java/example/day07/todo/DBCreate.sql
+++ src/main/java/example/day07/todo/DBCreate.sql
@@ -15,4 +15,4 @@ insert into todo(listname) values("테스트할일1");
 insert into todo(listname) values("테스트할일2");
 insert into todo(listname) values("테스트할일3");
 
-https://code-with-me.global.jetbrains.com/shif_FXplKqQL8dQgCvtIg#p=IC&fp=16E956FCB2878BEED96F29EF7517A7315C8AE1CE0EABDB455B7276F6A890E01E&newUi=true
+https://code-with-me.global.jetbrains.com/B90E1VKikUEaqek2ZNQqIQ#p=IC&fp=16E956FCB2878BEED96F29EF7517A7315C8AE1CE0EABDB455B7276F6A890E01E&newUi=true
