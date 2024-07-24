e59134202dd9921fbf421ea8538e25b97aaa8815
syeon8344 <yeon8344@gmail.com>
2024-07-24 17:49:31 +0900

    0724 boardWrite

--- src/main/resources/static/js/board/boardwrite.js
+++ src/main/resources/static/js/board/boardwrite.js
@@ -22,6 +22,25 @@ function getCategory(){
 }
 
 function _write(){
-    
+    //{"bcno" : bcno, "btitle" : btitle, "bcontent" : bcontent}
+    let btitle = document.querySelector("#btitle").value
+    let bcontent = document.querySelector("#bcontent").value
+    let bcno = document.querySelector("#category").value
+    let dto = {btitle : btitle, bcontent : bcontent, bcno : bcno}
+    $.ajax({
+        async : false,
+        method : "POST",
+        url : "/board/write",
+        data : JSON.stringify(dto),
+        contentType : "application/json",
+        success : resp => {
+            if (resp) {
+                alert("글쓰기 완료.")
+                location.href="/board/getall"
+            } else {
+                alert("글쓰기 오류.")
+            }
+        }
+    })
 }
 
